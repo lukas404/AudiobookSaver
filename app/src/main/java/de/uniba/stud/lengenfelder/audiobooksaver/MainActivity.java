c6a6d9d5ebc1a89646b5cf7246f8b3fb06050157
Lukas Lengenfelder <lukas-lothar.lengenfelder@stud.uni-bamberg.de>
2018-04-12 17:36:06 +0200

    added adapter, organized imports

--- app/src/main/java/de/uniba/stud/lengenfelder/audiobooksaver/MainActivity.java
+++ app/src/main/java/de/uniba/stud/lengenfelder/audiobooksaver/MainActivity.java
@@ -1,20 +1,22 @@
 package de.uniba.stud.lengenfelder.audiobooksaver;
 
-import android.nfc.Tag;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = AppDatabase.getInstance(getApplicationContext());
 
         FloatingActionButton addBtn = findViewById(R.id.addBtn);
         addBtn.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +25,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: pressed!");
             }
         });
+
     }
 }
