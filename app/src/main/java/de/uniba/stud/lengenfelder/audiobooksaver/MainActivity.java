e7625ec07164191fc38d046e5b4f00ee1c89ce9a
Lukas Lengenfelder <lukas-lothar.lengenfelder@stud.uni-bamberg.de>
2018-04-12 15:59:13 +0200

    added add activity and its layout, logged addBtn

--- app/src/main/java/de/uniba/stud/lengenfelder/audiobooksaver/MainActivity.java
+++ app/src/main/java/de/uniba/stud/lengenfelder/audiobooksaver/MainActivity.java
@@ -1,13 +1,27 @@
 package de.uniba.stud.lengenfelder.audiobooksaver;
 
+import android.nfc.Tag;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: pressed!");
+            }
+        });
     }
 }
