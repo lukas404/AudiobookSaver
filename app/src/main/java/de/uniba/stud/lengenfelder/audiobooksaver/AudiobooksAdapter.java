d1a5d03b6346f40fa46c5a062d1030ed04af40d8
Lukas Lengenfelder <lukas-lothar.lengenfelder@stud.uni-bamberg.de>
2018-04-13 11:07:20 +0200

    implemented adapter for audiobooks

--- app/src/main/java/de/uniba/stud/lengenfelder/audiobooksaver/AudiobooksAdapter.java
+++ app/src/main/java/de/uniba/stud/lengenfelder/audiobooksaver/AudiobooksAdapter.java
@@ -1,27 +1,52 @@
 package de.uniba.stud.lengenfelder.audiobooksaver;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class AudiobooksAdapter extends BaseAdapter {
+    List<Audiobook> audiobooks;
+    LayoutInflater mInflater;
+
+    public AudiobooksAdapter(Context context, List<Audiobook> audiobooks) {
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.audiobooks = audiobooks;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return audiobooks.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return audiobooks.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return audiobooks.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View view = mInflater.inflate(R.layout.main_list_view_detail, null);
+        TextView titleTextView = view.findViewById(R.id.titleTextView);
+        TextView descTextView = view.findViewById(R.id.descTextView);
+
+        String title = audiobooks.get(position).getTitle();
+        String desc = audiobooks.get(position).getDesc();
+
+        titleTextView.setText(title);
+        descTextView.setText(desc);
+
+        return view;
     }
 }
